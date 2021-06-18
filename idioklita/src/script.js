8f6523d4d058b330a9bebb27548892bc362498bf
DanielCasas21 <46403286+DanielCasas21@users.noreply.github.com>
2021-06-18 18:45:22 -0500

    Update script.js

--- idioklita/src/script.js
+++ idioklita/src/script.js
@@ -1,5 +1,25 @@
 var verbs = ["γράψιμο", "βάψιμο", "τρέξιμο", "ντύσιμο", "κρέας", "γάλα", "δόρυ", "γεγονός", "καθεστώς", "συμβάν", "παν", "σύμπαν", "φως", "δάκρυ", "βράδυ", "μηδέν", "φωνήεν", "πρωί", "οξύ"];
 
+var ousiastiko = {ellinika:"γράψιμο", english:"writing."};
+var ousiastiko1 = {ellinika:"βάψιμο", english:"baptism."};
+var ousiastiko2 = {ellinika:"τρέξιμο", english:"running. Η αγαπημένη μας λέξη!"};
+var ousiastiko3 = {ellinika:"ντύσιμο", english:"formal suit."};
+var ousiastiko4 = {ellinika:"κρέας", english:"meat."};
+var ousiastiko5 = {ellinika:"γάλα", english:"milk."};
+var ousiastiko6 = {ellinika:"δόρυ", english:"spear. 'Hasta, hasta'στα Λατινικά!"};
+var ousiastiko7 = {ellinika:"γεγονός", english:"incident, event, fact."};
+var ousiastiko8 = {ellinika:"καθεστώς", english:"regime."};
+var ousiastiko9 = {ellinika:"συμβάν", english:"incident."};
+var ousiastiko10 = {ellinika:"παν", english:"everything."};
+var ousiastiko11 = {ellinika:"σύμπαν", english:"the Universe."};
+var ousiastiko12 = {ellinika:"φως", english:"light."};
+var ousiastiko13 = {ellinika:"δάκρυ", english:"tear."};
+var ousiastiko14 = {ellinika:"βράδυ", english:"evening. In Greece form 8pm till midnight!!!"};
+var ousiastiko15 = {ellinika:"μηδέν", english:"zero."};
+var ousiastiko16 = {ellinika:"φωνήεν", english:"vowel."};
+var ousiastiko17 = {ellinika:"πρωί", english:"morning."};
+var ousiastiko18 = {ellinika:"οξύ", english:"acid."};
+
 function randGen() {
   return Math.floor(Math.random() * 5);
 }
@@ -8,6 +28,86 @@ function autoFill() {
   var rand3 = Math.floor(Math.random() * verbs.length);
   var content = verbs[rand3] 
   document.getElementById('input1').value = content;
+
+  if ( content === ousiastiko.ellinika ) {
+    document.getElementById("input2").innerHTML = "- " + ousiastiko.ellinika + " στα αγγλικά είναι: " + ousiastiko.english;
+  }
+
+  else if (content === ousiastiko1.ellinika){
+    document.getElementById("input2").innerHTML = "- " + ousiastiko1.ellinika + " στα αγγλικά είναι: " + ousiastiko1.english;
+  }
+
+  else if (content === ousiastiko2.ellinika){
+    document.getElementById("input2").innerHTML = "- " + ousiastiko2.ellinika + " στα αγγλικά είναι: " + ousiastiko2.english;
+  }
+
+  else if ( content === ousiastiko3.ellinika ) {
+    document.getElementById("input2").innerHTML = "- " + ousiastiko3.ellinika + " στα αγγλικά είναι: " + ousiastiko3.english;
+  }
+
+  else if ( content === ousiastiko4.ellinika ) {
+    document.getElementById("input2").innerHTML = "- " + ousiastiko4.ellinika + " στα αγγλικά είναι: " + ousiastiko4.english;
+  }
+
+  else if ( content === ousiastiko5.ellinika ) {
+    document.getElementById("input2").innerHTML = "- " + ousiastiko5.ellinika + " στα αγγλικά είναι: " + ousiastiko5.english;
+  }
+
+  else if ( content === ousiastiko6.ellinika ) {
+    document.getElementById("input2").innerHTML = "- " + ousiastiko6.ellinika + " στα αγγλικά είναι: " + ousiastiko6.english;
+  }
+
+  else if( content === ousiastiko7.ellinika ) {
+    document.getElementById("input2").innerHTML = "- " + ousiastiko7.ellinika + " στα αγγλικά είναι: " + ousiastiko7.english;
+  }
+
+  else if ( content === ousiastiko8.ellinika ) {
+    document.getElementById("input2").innerHTML = "- " + ousiastiko8.ellinika + " στα αγγλικά είναι: " + ousiastiko8.english;
+  }
+
+  else if( content === ousiastiko9.ellinika ) {
+    document.getElementById("input2").innerHTML = "- " + ousiastiko9.ellinika + " στα αγγλικά είναι: " + ousiastiko9.english;
+  }
+
+  else if ( content === ousiastiko10.ellinika ) {
+    document.getElementById("input2").innerHTML = "- " + ousiastiko10.ellinika + " στα αγγλικά είναι: " + ousiastiko10.english;
+  }
+
+  else if( content === ousiastiko11.ellinika ) {
+    document.getElementById("input2").innerHTML = "- " + ousiastiko11.ellinika + " στα αγγλικά είναι: " + ousiastiko11.english;
+  }
+
+  else if ( content === ousiastiko12.ellinika ) {
+    document.getElementById("input2").innerHTML = "- " + ousiastiko12.ellinika + " στα αγγλικά είναι: " + ousiastiko12.english;
+  }
+
+  else if ( content === ousiastiko13.ellinika ) {
+    document.getElementById("input2").innerHTML = "- " + ousiastiko13.ellinika + " στα αγγλικά είναι: " + ousiastiko13.english;
+  }
+
+  else if ( content === ousiastiko14.ellinika ) {
+    document.getElementById("input2").innerHTML = "- " + ousiastiko14.ellinika + " στα αγγλικά είναι: " + ousiastiko14.english;
+  }
+
+  else if ( content === ousiastiko15.ellinika ) {
+    document.getElementById("input2").innerHTML = "- " + ousiastiko15.ellinika + " στα αγγλικά είναι: " + ousiastiko15.english;
+  }
+
+  else if ( content === ousiastiko16.ellinika ) {
+    document.getElementById("input2").innerHTML = "- " + ousiastiko16.ellinika + " στα αγγλικά είναι: " + ousiastiko16.english;
+  }
+
+  else if ( content === ousiastiko17.ellinika ) {
+    document.getElementById("input2").innerHTML = "- " + ousiastiko17.ellinika + " στα αγγλικά είναι: " + ousiastiko17.english;
+  }
+
+  else if ( content === ousiastiko18.ellinika ) {
+    document.getElementById("input2").innerHTML = "- " + ousiastiko18.ellinika + " στα αγγλικά είναι: " + ousiastiko18.english;
+  }
+
+  else {
+    document.getElementById("input2").innerHTML = "Κρίμα, δεν το βρήκαμε!"
+  }
 }
 
 function myFunction() {
@@ -158,8 +258,8 @@ function myFunction() {
     declension6 = "τα " + x.slice(0,-1) + "έα.";
     declension7 = x + "νά!";
   }
-  
-    else if (noun === "οξύ"){
+
+  else if (noun === "οξύ"){
     declension = "το " + noun + ".";
     declension1 = "του " + x.slice(0,-1) + "έος.";
     declension2 = "το " + noun + ".";
@@ -169,8 +269,8 @@ function myFunction() {
     declension6 = "τα " + x.slice(0,-1) + "έα.";
     declension7 = x + "νά!";
   }
-  
-else {
+
+  else {
     declension = "Ονομαστική";
     declension1 = "Γενική";
     declension2 = "Αιτιατική";
@@ -180,7 +280,7 @@ else {
     declension6 = "Αιτιατική";
     declension7 = "Κλιτική";
   }
-  
+
   document.getElementById("demo").innerHTML = declension;
   document.getElementById("demo1").innerHTML = declension1;
   document.getElementById("demo2").innerHTML = declension2;
